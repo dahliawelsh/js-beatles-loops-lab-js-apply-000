e87b1c8b16cd3db7a2abf9ed65b92c939cbff564
Dahlia Welsh <dahliawelsh@gmail.com>
2019-10-14 21:44:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,13 +14,3 @@ function iLoveTheBeatles(num) {
 
     return love;
 }
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
-    }
-    return newFacts;
-}
